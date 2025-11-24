820acceb88000028bb0a48b57a81c2218033ffdf
hasibforazitech-hue <hasib.forazitech@gmail.com>
2025-11-24 11:05:35 +0600

    icon upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div id='footer' className='bg-black'>
+            <div className='content-wrap md:pt-24 pb-8 pt-16 flex flex-col lg:flex-row gap-6 flex-no-wrap justify-between items-center'>
+                <div className='flex text-white justify-between w-full gap'>
+                    <div className='w-8/12'>
+                        <div className='flex items-center justify-start gap-3'>
+                            <img className='w-[140px]' src="../../public/assets/images/logo.webp" alt="logo" />
+                            <a href="#">Terms of Use</a>
+                            <a href="#">Terms of Use</a>
+                            <a href="#">Terms of Use</a>
+                        </div>
+                        <div className='flex items-center justify-start gap-3 my-4'>
+                            <a href="#">SVG</a>
+                            <a href="#">SVG</a>
+                            <a href="#">SVG</a>
+                        </div>
+                    </div>
+                    <div className='w-4/12 text-end'>
+                        <div>
+                            <a href="#">SVG</a>
+                            <a href="#">SVG</a>
+                            <a href="#">SVG</a>
+                            <a href="#">SVG</a>
+                            <a href="#">SVG</a>
+                        </div>
+                        <p className='sub-title'>© 2024 ME3 LABS INC | ALL RIGHTS RESERVED</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
---
+++ src/components/Icons/IconTwitter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IconTwitter = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 28 26" fill="none">
+            <path d="M21.6578 0.5H25.8746L16.662 11.0897L27.4999 25.4999H19.0139L12.3674 16.7602L4.76234 25.5H0.54296L10.3968 14.1731L0 0.5H8.70133L14.7092 8.48853L21.6578 0.5ZM20.1779 22.9615H22.5145L7.43171 2.90511H4.92426L20.1779 22.9615Z" fill="currentcolor" />
+        </svg>
+    )
+}
+
+export default IconTwitter
