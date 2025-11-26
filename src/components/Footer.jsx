17393af810d5aac81ddba98e9c41070c4a293034
hasibforazitech-hue <hasib.forazitech@gmail.com>
2025-11-26 12:16:40 +0600

    image update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,8 @@ import IconYoutube from './Icons/IconYoutube';
 import IconMail from './Icons/IconMail';
 import footerImageOne from '../../public/assets/images/footer-img-one.svg';
 import footerImageTwo from '../../public/assets/images/footer-image-two.webp';
-import footerImageOne from '../../public/assets/images/footer-img-one.svg';
+import footerImageThree from '../../public/assets/images/footer_image_three.svg';
+import logo from '../../public/assets/images/logo.webp';
 
 
 const Footer = () => {
@@ -19,7 +20,7 @@ const Footer = () => {
                 <div className='flex flex-col md:flex-row text-white justify-between w-full gap-6'>
                     <div className='w-full md:w-8/12'>
                         <div className='flex flex-row flex-wrap items-center justify-start gap-3'>
-                            <a href="/"><img className='w-[140px] block' src={footerImageOne} alt="logo" type='webp' /></a>
+                            <a href="/"><img className='w-[140px] block' src={logo} alt="Logo" /></a>
                             <div className='flex flex-row flex-wrap items-center justify-start gap-3'>
                                 <a className='hover:underline text-arial text-sm leading-6' href="https://help.me3.io/en/articles/9245378-terms-of-use">Terms of Use</a>
                                 <a className='hover:underline text-arial text-sm leading-6' href="https://help.me3.io/en/articles/9245363-privacy-policy">Privacy Policy</a>
@@ -27,9 +28,9 @@ const Footer = () => {
                             </div>
                         </div>
                         <div className='flex items-center justify-start gap-4 my-4'>
-                            <a href="https://projects.assuredefi.com/project/me3"><img className='h-10' src={footerImageTwo} alt="footer-img-one" type='svg' /></a>
+                            <a href="https://projects.assuredefi.com/project/me3"><img className='h-10' src={footerImageOne} alt="footer-img-one" type='svg' /></a>
                             <a href="https://github.com/0xGuard-com/audit-reports/blob/master/Me3/Me3.pdf"><img className='h-10' src={footerImageTwo} alt="footer-image-two" type='pdf' /></a>
-                            <a href="https://bscscan.com/token/0x42b4daa9210102707373f4618852f926a4150277"><img className='h-10' src='/public/assets/images/footer_image_three.svg' alt="footer_image_three" type='svg' /></a>
+                            <a href="https://bscscan.com/token/0x42b4daa9210102707373f4618852f926a4150277"><img className='h-10' src={footerImageThree} alt="footer_image_three" type='svg' /></a>
                         </div>
                     </div>
                     <div className='w-full md:w-4/12'>
